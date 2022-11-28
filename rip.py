9db0ea8bbe4d6576a4d07f5325736d8ac8c30ee6
kabutor <43006263+kabutor@users.noreply.github.com>
2022-11-28 16:49:48 +0100

    bugs bugs bugs

--- rip.py
+++ rip.py
@@ -8,7 +8,7 @@ headers={"User-Agent":"Mozilla/5.0 (X11; Linux x86_64; rv:107.0) Gecko/20100101 
 latest_title = {}
 
 # podcast to download (use URL)
-podcasts ={ 'LODE':'https://www.ivoox.com/en/podcast-orbita-de-endor-podcast_sq_f113302_1.html',
+podcasts ={ 'LODE':'https://www.ivoox.com/podcast-orbita-de-endor-podcast_sq_f113302_1.html',
            'CAMPKRYP':'https://www.ivoox.com/podcast-campamento-krypton_sq_f167429_1.html' }
 # Output directory
 mp3_dir = '/srv/http/mp3/'
@@ -50,10 +50,11 @@ def get_audio_link(_name , _data):
     headers = { 'Referer' : mp3_new_url, 'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:107.0) Gecko/20100101 Firefox/107.0'}
     content_mp3 = requests.get(mp3_new_url, headers=headers)# ,proxies=proxies, verify=False) #, allow_redirects=False)
     if content_mp3.status_code != 401:
-        #save mp3
-        _title = _title.replace('#','_')
-        _title = _title.replace(':','_')
-        with open( os.path.join(mp3_dir , (_title[:60].replace(" ","_")) +'.mp3'), 'wb') as f:
+        #save mp3 title
+        _title_tosave = _title.replace('#','_')
+        _title_tosave = _title_tosave.replace(':','_')
+        _title_tosave = _title_tosave.replace(" ","_")
+        with open( os.path.join(mp3_dir , (_title_tosave[:60]) +'.mp3'), 'wb') as f:
             f.write(content_mp3.content)
     # save as latest title
     with open( 'ivoorip.txt', 'w' ) as f:
@@ -74,13 +75,13 @@ for podcast_name, podcast_url in podcasts.items():
         if podcast_name in latest_title:
             if latest_title[podcast_name] == ep[0]:
                 break
-        else:
-            #print(ep[0])
-            new_ep.append(ep) 
+            else:
+                print("Queue %s" % ep)
+                new_ep.append(ep) 
 
 
     # get episodes     
     for eps in reversed(new_ep):
-        print("Getting %s" % eps[0])
+        print("Downloading %s" % eps[0])
         get_audio_link(podcast_name, eps)
         
